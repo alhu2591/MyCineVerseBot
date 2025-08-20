17ed4693a92c31f299c12a260b8d34a206a1a10c
alhu2591 <143001551+alhu2591@users.noreply.github.com>
2025-08-20 10:08:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+import os
+
 # --- إعدادات تليجرام ---
-TELEGRAM_TOKEN = '8408828754:AAHpdcc9N3jeLi0G6yibpkLiFzqBZiuqDQc'
-CHANNEL_ID = '@MyCineVerse_bot' # اختياري، للقناة
+# يقرأ التوكن من المتغيرات السرية في Render
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHANNEL_ID = '@MyCineVerse_bot'
 
 # --- مفاتيح API ---
-TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzkyNDI4OGU2OTMwYzMxNGEzNGZjZTlmZjNiM2UzMCIsIm5iZiI6MTc1NTY3MzA3OS4yNDg5OTk4LCJzdWIiOiI2OGE1NzFmNzU5ZDEzODZkODM1ZGQyOGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ezCDGh1yZzDavB8ChxbCh6K9VnAN8EkVxbpP4_gquYw'
+# يقرأ مفتاح TMDb من المتغيرات السرية في Render
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
 
 # --- إعدادات المدير ---
-ADMIN_CHAT_ID = 7502561234 # ‼️ تم التحديث
+# يقرأ ID المدير من المتغيرات السرية في Render
+ADMIN_CHAT_ID = int(os.environ.get('ADMIN_CHAT_ID', 0))

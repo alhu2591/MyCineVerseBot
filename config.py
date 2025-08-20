3484f33dad1eab7735f483b0432f72e96971c835
alhu2591 <143001551+alhu2591@users.noreply.github.com>
2025-08-20 11:14:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
-
 # --- إعدادات تليجرام ---
-# يقرأ التوكن من المتغيرات السرية في Render
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-CHANNEL_ID = '@MyCineVerse_bot'
+# ‼️ ضع هنا التوكن الجديد الذي حصلت عليه بعد عمل /revoke
+TELEGRAM_TOKEN = '8408828754:AAGgVrKnFPWSVpssc3B5XWUM_KomVT7lHQc'
+CHANNEL_ID = '@MyCineVerse_bot' # القناة التي سيتم النشر فيها
 
 # --- مفاتيح API ---
-# يقرأ مفتاح TMDb من المتغيرات السرية في Render
-TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
+# ‼️ تأكد من استخدام مفتاح API (v3 auth) وليس (v4 auth)
+TMDB_API_KEY = 'b7924288e6930c314a34fce9ff3b3e30'
 
 # --- إعدادات المدير ---
-# يقرأ ID المدير من المتغيرات السرية في Render
-ADMIN_CHAT_ID = int(os.environ.get('ADMIN_CHAT_ID', 0))
+ADMIN_CHAT_ID = 7502561234 # الـ ID الخاص بك
